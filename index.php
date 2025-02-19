4e426042bb7925d38d1d7ca9a0d2e2a04d2adaf1
Tsilavina <garry@softibox.com>
2025-02-19 11:25:33 +0300

    Creating upload file

--- index.php
+++ index.php
@@ -3,10 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require __DIR__ . '/vendor/autoload.php'; // Charge l'autoload de Composer
-include_once  __DIR__ . '/utils/function.php';
 use VSC\API\VirusCampAPI;
 $ViruscampAPI = new VirusCampAPI();
-dumpThis($ViruscampAPI->scanFile(__DIR__ .'/image/11411.jpg'));
+dumpThis($ViruscampAPI->scanFile(__DIR__ .'/uploads/11411.jpg'));
 
 ?>
 
